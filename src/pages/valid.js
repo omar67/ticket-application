afe06122569e8c80e9e679db4cb4a6e79dc5e164
Omar <omarramo.bs@gmail.com>
2021-07-27 14:01:22 +0300

    Added QR code image

--- src/pages/valid.js
+++ src/pages/valid.js
@@ -1,35 +1,38 @@
 import React from "react";
 
 export default function Valid(props) {
-  let { title, description, price } = props.props.ticket;
+  let { title, description, price, qr } = props.props.ticket;
   return (
-    <table>
-      <tbody>
-        <tr>
-          <td>
-            <h1>Title:</h1>
-          </td>
-          <td>
-            <h1>{title}</h1>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <h1>Description:</h1>
-          </td>
-          <td>
-            <h1>{description}</h1>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <h1>Price:</h1>
-          </td>
-          <td>
-            <h1>{price}</h1>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <h1>Title:</h1>
+            </td>
+            <td>
+              <h1>{title}</h1>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h1>Description:</h1>
+            </td>
+            <td>
+              <h1>{description}</h1>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h1>Price:</h1>
+            </td>
+            <td>
+              <h1>{price}</h1>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <img src={qr} alt="Claim QR Code" />
+    </div>
   );
 }
